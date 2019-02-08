47c98a00d2d1e9e92416a254ee04e2757619ea22
mkoyfman <5942777+mkoyfman@users.noreply.github.com>
2019-02-08 14:44:32 -0600

    added check for local OpenSSL presence and augment that CA bundle with Netskope CA

--- ns_certbundle.sh
+++ ns_certbundle.sh
@@ -6,6 +6,8 @@ echo "trying to check if certs exist"
 cacert='/Library/Application Support/Netskope/STAgent/data/'nscacert.pem
 tenantcert='/Library/Application Support/Netskope/STAgent/data/'nstenantcert.pem
 certbundle='./'netskope-cert-bundle.pem
+opensslbundle=/usr/local/etc/openssl/cert.pem
+opensslbundlebak=/usr/local/etc/openssl/cert.pem.bak
 
 
 if [ -f "$cacert" ] && [ -f "$tenantcert" ]
@@ -31,6 +33,12 @@ then
        else
            echo "Error!!"
        fi
+       if [ -f "$opensslbundle" ]
+        then 
+          cp $opensslbundle $opensslbundlebak
+          cat "$tenantcert" "$cacert">> "$opensslbundle"
+          echo "Added Netskope CA bundle to local OpenSSL CA in $opensslbundle"
+        fi
     fi
 else
     echo "Required certs not found. Please check if Netskope Client is installed"
